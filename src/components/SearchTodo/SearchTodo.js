ff39ce2b7be57f0ef493a5bf31245a4b1eaf2745
Scentjuls <chidynwanya11@gmail.com>
2023-09-04 16:12:10 +0100

    used useReducer to manage 2 states

--- src/components/SearchTodo/SearchTodo.js
+++ src/components/SearchTodo/SearchTodo.js
@@ -1,103 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import "./SearchTodo.css";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "color":
+            return { ...state, color:  !state.color }
+        case "search":
+            return { ...state, search: action.payload }
+        default:
+            throw new Error();
+    }
+}
 
 export const SearchTodo = (props) => {
     const { todos, setTodos, onSetTodos } = props;
-    const [search, setSearch] = useState("");
+
+    const [state, dispatch] = useReducer(reducer, {color: false, search: ""})
 
     useEffect(() => {
-        if (!search) {
+        if (!state.search) {
             setTodos(todos);
             console.log("hwere", todos)
             localStorage.setItem("tasks", JSON.stringify(todos));
         }
-    },[search, todos, setTodos])
+    },[todos, setTodos, state.search])
 
     const searchHandle = () => {
-        if (!search) {
+        if (!state.search) {
             return
         }
+        console.log("new")
         const todosReplica = [...todos];
-        const newArray = todosReplica.filter(todo => todo.task === search);
+        const newArray = todosReplica.filter(todo => todo.task === state.search);
+        
         localStorage.setItem("search", JSON.stringify(newArray));
         onSetTodos(true);
     }
 
     const clearSearch = () => {
-        setSearch("");
+        dispatch({type: "search", payload: ""});
     }
 
   return (
-      <div className="search-container">
+      <div className="search-container" style={{backgroundColor: state.color ? "#FFF952" : "#FFF"}}>
           <input
               type="text"
               className="search"
               placeholder="search for a task..."
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
+              value={state.search}
+              onChange={(e) => dispatch({type: "search", payload: e.target.value})}
           />
           {
-              search && (
+              state.search ? (
                   <button className="clear" onClick={clearSearch}>X</button>
+              ) : (
+                  <button className="btn" onClick={(() => dispatch({type: "color"}))}>Color</button>    
               )
           }
           <button className="btn" onClick={searchHandle}>search</button>
     </div>
   )
 }
-
-
-// import React, { useEffect, useReducer } from 'react';
-// import "./SearchTodo.css";
-
-// const reducer = (state, action) => {
-//     switch (action.type) {
-//         case "color":
-//             return { ...state, color:  !state.color }
-//         case "search":
-//             return { ...state, search: action.payload }
-//         default:
-//             throw new Error();
-//     }
-// }
-
-// export const SearchTodo = (props) => {
-//     const { todos, setTodos, onSetTodos } = props;
-
-//     const [state, dispatch] = useReducer(reducer, {color: false, search: ""})
-//     // const [search, setSearch] = useState("");
-//     // const [color, setColor] = useState(false);
-
-//     useEffect(() => {
-//         if (!state.search) {
-//             setTodos(todos);
-//             console.log("hwere", todos)
-//             localStorage.setItem("tasks", JSON.stringify(todos));
-//         }
-//     },[todos, setTodos, state.search])
-
-//     const searchHandle = () => {
-//         if (!state.search) {
-//             return
-//         }
-//         const todosReplica = [...todos];
-//         const newArray = todosReplica.filter(todo => todo.task === state.search);
-//         console.log("new", newArray)
-//         localStorage.setItem("search", JSON.stringify(newArray));
-//         onSetTodos(true);
-//     }
-
-//   return (
-//       <div className="search-container" style={{backgroundColor: state.color ? "#FFF952" : "#FFF"}}>
-//           <input
-//               type="text"
-//               className="search"
-//               placeholder="search for a task..."
-//               value={state.search}
-//               onChange={(e) => dispatch({type: "search"})}
-//           />
-//           <button className="btn" onClick={searchHandle}>search</button>
-//           <button className="btn" onClick={(() => dispatch({type: "color"}))}>Color</button>
-//     </div>
-//   )
-// }
