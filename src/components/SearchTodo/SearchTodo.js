17b436c4ab57f11d5c97766ad05f73c340e25fb8
Scentjuls <chidynwanya11@gmail.com>
2023-09-04 15:24:39 +0100

    added the clear search button to clear search input

--- src/components/SearchTodo/SearchTodo.js
+++ src/components/SearchTodo/SearchTodo.js
@@ -23,6 +23,10 @@ export const SearchTodo = (props) => {
         onSetTodos(true);
     }
 
+    const clearSearch = () => {
+        setSearch("");
+    }
+
   return (
       <div className="search-container">
           <input
@@ -32,7 +36,68 @@ export const SearchTodo = (props) => {
               value={search}
               onChange={(e) => setSearch(e.target.value)}
           />
+          {
+              search && (
+                  <button className="clear" onClick={clearSearch}>X</button>
+              )
+          }
           <button className="btn" onClick={searchHandle}>search</button>
     </div>
   )
 }
+
+
+// import React, { useEffect, useReducer } from 'react';
+// import "./SearchTodo.css";
+
+// const reducer = (state, action) => {
+//     switch (action.type) {
+//         case "color":
+//             return { ...state, color:  !state.color }
+//         case "search":
+//             return { ...state, search: action.payload }
+//         default:
+//             throw new Error();
+//     }
+// }
+
+// export const SearchTodo = (props) => {
+//     const { todos, setTodos, onSetTodos } = props;
+
+//     const [state, dispatch] = useReducer(reducer, {color: false, search: ""})
+//     // const [search, setSearch] = useState("");
+//     // const [color, setColor] = useState(false);
+
+//     useEffect(() => {
+//         if (!state.search) {
+//             setTodos(todos);
+//             console.log("hwere", todos)
+//             localStorage.setItem("tasks", JSON.stringify(todos));
+//         }
+//     },[todos, setTodos, state.search])
+
+//     const searchHandle = () => {
+//         if (!state.search) {
+//             return
+//         }
+//         const todosReplica = [...todos];
+//         const newArray = todosReplica.filter(todo => todo.task === state.search);
+//         console.log("new", newArray)
+//         localStorage.setItem("search", JSON.stringify(newArray));
+//         onSetTodos(true);
+//     }
+
+//   return (
+//       <div className="search-container" style={{backgroundColor: state.color ? "#FFF952" : "#FFF"}}>
+//           <input
+//               type="text"
+//               className="search"
+//               placeholder="search for a task..."
+//               value={state.search}
+//               onChange={(e) => dispatch({type: "search"})}
+//           />
+//           <button className="btn" onClick={searchHandle}>search</button>
+//           <button className="btn" onClick={(() => dispatch({type: "color"}))}>Color</button>
+//     </div>
+//   )
+// }
